49d517cdf694f404443d1c5ee70c23db37949111
Himanshu Sekhar Nayak <himanshuwindows8.1@gmail.com>
2025-04-05 17:42:33 +0530

    Added input embedding and positional encoding

--- model.py
+++ model.py
@@ -2,6 +2,45 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
+class InputEmbedding(nn.Module):
+    def __init__(self, d_model, vocab_size):
+        super().__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(vocab_size, d_model)
+
+    def forward(self, x):
+        return self.embedding(x) * torch.sqrt(self.d_model)
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, seq_len, dropout):
+        super().__init()
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout(dropout)
+
+        pe = torch.zeros(seq_len, d_model)
+        position = torch.arange(0, self.seq_len, dtype=torch.float32).unsqueeze(1)
+        div_term = torch.exp(
+            torch.arange(0, d_model, 2).float() * (-torch.log(10000.0) / self.d_model)
+        )
+
+        # Apply sin to even pos
+        pe[:, 0::2] = torch.sin(position * div_term)
+        # Apply cos to odd pos
+        pe[:, 1::2] = torch.cos(position * div_term)
+
+        # Apply batch (1, seq_len, d_model)
+        pe = pe.unsqueeze(0)
+        self.register_buffer("pe", pe)
+
+    def forward(self, x):
+        x = x + (self.pe[:, : x.shape[1], :]).requires_grad_(False)
+        return self.dropout(x)
+
+
 class MultiHeadAttention(nn.Module):
     def __init__():
         pass
